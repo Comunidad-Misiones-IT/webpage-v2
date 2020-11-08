c92051b293fcee403712539d803115bc6cf0d408
Nataya Soledad <natayafs@gmail.com>
2020-11-08 09:16:11 -0300

    Estilo de la seccion "eventos" y sus componentes

--- src/Eventos.js
+++ src/Eventos.js
@@ -3,27 +3,19 @@ import React, { Component } from "react";
 class Eventos extends Component {
   render() {
     return (
-      <div className="md:flex">
-        <div className="md:flex-shrink-0">
-          <img
-            className="rounded-lg md:w-56"
-            src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
-            alt="Woman paying for a purchase"
-          />
-        </div>
-        <div className="mt-4 md:mt-0 md:ml-6">
-          <div className="uppercase tracking-wide text-sm text-indigo-600 font-bold">
-            Miércoles 20 de noviembre / 19:30hs
+      <section className="text-gray-700 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Eventos próximos</h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+              ¡Enterate de todas las novedades!
+            </p>
           </div>
-          <div className="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">
-            Charla de introducción a Figma
+          <div className="container">
+            
           </div>
-          <p className="mt-2 text-gray-600">
-            Ulises nos estará introduciendo en esta fantástica herramienta de
-            diseño colaborativa. Para saber más escribinos a hola@mit.ar
-          </p>
         </div>
-      </div>
+      </section>
     );
   }
 }
