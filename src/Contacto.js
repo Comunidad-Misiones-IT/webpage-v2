c267a8387b589cde37ed43d1708c54e10e0acc74
Nataya Soledad <natayafs@gmail.com>
2020-11-09 01:44:08 -0300

    Update Contacto.js

--- src/Contacto.js
+++ src/Contacto.js
@@ -8,11 +8,89 @@ class Contacto extends Component {
           <div className="flex flex-col text-center w-full mb-20">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contactanos</h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-              sdahsdsdasjhdgajsgdsj
+              ¡Sigue nuestras redes!
             </p>
           </div>
           <div className="flex flex-wrap -m-2">
-            <p>Hello World</p>
+
+            <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div class="bg-purple-100 h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <a class="m-3 text-gray-500">
+                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <div class="m-3 flex-grow">
+                  Twitter
+                </div>
+              </div>
+            </div>
+
+            <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div class="bg-purple-100 h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <a class="m-3 text-gray-500">
+                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <div class="m-3 flex-grow">
+                  Twitter
+                </div>
+              </div>
+            </div>
+
+            <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div class="bg-purple-100 h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <a class="m-3 text-gray-500">
+                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <div class="m-3 flex-grow">
+                  Twitter
+                </div>
+              </div>
+            </div>
+
+            <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div class="bg-purple-100 h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <a class="m-3 text-gray-500">
+                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <div class="m-3 flex-grow">
+                  Twitter
+                </div>
+              </div>
+            </div>
+
+            <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div class="bg-purple-100 h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <a class="m-3 text-gray-500">
+                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <div class="m-3 flex-grow">
+                  Twitter
+                </div>
+              </div>
+            </div>
+
+            <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div class="bg-purple-100 h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <a class="m-3 text-gray-500">
+                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <div class="m-3 flex-grow">
+                  Twitter
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </section>
