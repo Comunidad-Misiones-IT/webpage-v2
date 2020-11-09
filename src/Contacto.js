386ef5d87a95a4892eafae5589f349124c176845
Nataya Soledad <natayafs@gmail.com>
2020-11-09 02:46:04 -0300

    Diseño de links de redes sociales

--- src/Contacto.js
+++ src/Contacto.js
@@ -14,8 +14,8 @@ class Contacto extends Component {
           <div className="flex flex-wrap -m-2">
 
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div class="bg-purple-100 h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <a class="m-3 text-gray-500">
+              <div class="card-mit h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <a class="m-3 text-gray-100">
                   <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                   </svg>
@@ -27,8 +27,8 @@ class Contacto extends Component {
             </div>
 
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div class="bg-purple-100 h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <a class="m-3 text-gray-500">
+              <div class="card-mit h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <a class="m-3 text-gray-100">
                   <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                   </svg>
@@ -40,8 +40,8 @@ class Contacto extends Component {
             </div>
 
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div class="bg-purple-100 h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <a class="m-3 text-gray-500">
+              <div class="card-mit h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <a class="m-3 text-gray-100">
                   <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                   </svg>
@@ -53,8 +53,8 @@ class Contacto extends Component {
             </div>
 
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div class="bg-purple-100 h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <a class="m-3 text-gray-500">
+              <div class="card-mit h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <a class="m-3 text-gray-100">
                   <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                   </svg>
@@ -66,8 +66,8 @@ class Contacto extends Component {
             </div>
 
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div class="bg-purple-100 h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <a class="m-3 text-gray-500">
+              <div class="card-mit h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <a class="m-3 text-gray-100">
                   <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                   </svg>
@@ -79,8 +79,8 @@ class Contacto extends Component {
             </div>
 
             <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div class="bg-purple-100 h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <a class="m-3 text-gray-500">
+              <div class="card-mit h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <a class="m-3 text-gray-100">
                   <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                   </svg>
