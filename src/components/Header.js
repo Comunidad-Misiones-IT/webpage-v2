1577cf36213f3dfaa6f3c91d032abbc497c5bf0d
Nataya Soledad <natayafs@gmail.com>
2021-06-11 20:54:26 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 import logo from "../assets/mit2.png";
 export default function Header() {
   return (
-        <header className="text-gray-600 body-font bg-mit">
-          <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <header className="shadow-xl text-gray-600 body-font bg-mit">
+          <div className="container mx-auto flex flex-wrap py-3 px-6 flex-col md:flex-row items-center">
             <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" to="/">
             <img
               src={logo}
@@ -13,7 +13,7 @@ export default function Header() {
             </Link>
             <nav 
             id="site-menu" 
-            class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center"
+            className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center"
             >
             <Link
               to="/"
@@ -49,9 +49,13 @@ export default function Header() {
             </nav>
               <Link
                 to="/live"
-                className="w-full md:w-auto text-center rounded px-4 py-2 bg-red-600"
+                className="w-full md:w-auto text-center rounded px-4 py-2 bg-gray-100"
               >
-                <b className="text-white">En vivo</b>
+                <b className="text-gray-900">
+                  <span className="text-red-600">● </span>
+                  En vivo
+                
+                </b>
               </Link>
           </div>
         </header>
